c9a8c719281be55ed45a42deddfe5b6b59ae3b0d
manojkumar <manojkumar.r@hepl.com>
2025-02-14 17:11:16 +0530

    first commit

---
+++ src/main/java/com/back/snail_be/SnailBeApplication.java
@@ -0,0 +1,13 @@
+package com.back.snail_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/snail_be/SnailBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.snail_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
